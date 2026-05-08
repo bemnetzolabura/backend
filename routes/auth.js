2ac2b331a61539c6a0d2ed5d7642b4ce8cc0b172
Bemnet Abebaw <bemnetzolabura@gmail.com>
2026-05-08 20:15:42 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const { signup, login, getMe } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
-
 const router = express.Router();
-
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/me', protect, getMe);
